f40c78c9621f7a4b26b9fdacf21fc754b71e4c0c
alxkhvn <alxkhvn@gmail.com>
2023-01-24 11:50:05 +0600

    nested serializer optimization

--- service/services/views.py
+++ service/services/views.py
@@ -9,6 +9,7 @@ from services.serializers import SubscriptionSerializer
 class SubscriptionView(ReadOnlyModelViewSet):
     # queryset = Subscription.objects.all().prefetch_related('client').prefetch_related('client__user')
     queryset = Subscription.objects.all().prefetch_related(
+        'plan',  # для оптимизации вложенного серализатора(проблема n+1)
         Prefetch('client', queryset=Client.objects.all().select_related('user').only('company_name', 'user__email')))
     serializer_class = SubscriptionSerializer
 
