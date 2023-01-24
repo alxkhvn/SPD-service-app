06c018b452af016235fa1c80d291d845515b223d
alxkhvn <alxkhvn@gmail.com>
2023-01-24 14:24:52 +0600

    annotate, aggregate

--- service/services/views.py
+++ service/services/views.py
@@ -1,4 +1,4 @@
-from django.db.models import Prefetch
+from django.db.models import Prefetch, F, Sum
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from clients.models import Client
@@ -10,7 +10,19 @@ class SubscriptionView(ReadOnlyModelViewSet):
     # queryset = Subscription.objects.all().prefetch_related('client').prefetch_related('client__user')
     queryset = Subscription.objects.all().prefetch_related(
         'plan',  # для оптимизации вложенного серализатора(проблема n+1)
-        'service',
-        Prefetch('client', queryset=Client.objects.all().select_related('user').only('company_name', 'user__email')))
+        Prefetch('client', queryset=Client.objects.all().select_related('user').only('company_name',
+                                                                                     'user__email')))\
+        .annotate(price=F('service__full_price') -
+                        F('service__full_price') * F('plan__discount_percent') / 100.0)
     serializer_class = SubscriptionSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        response = super().list(request, *args, **kwargs)
+
+        response_data = {'result': response.data}
+        response_data['total_amount'] = queryset.aggregate(total=Sum('price')).get('total')
+        response.data = response_data
+        return response
+
 
