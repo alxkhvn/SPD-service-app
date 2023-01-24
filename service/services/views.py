e836aeaa658be6b562b06f81180a9f3d0853baa0
alxkhvn <alxkhvn@gmail.com>
2023-01-24 12:09:48 +0600

    using SerializerMethodField

--- service/services/views.py
+++ service/services/views.py
@@ -10,6 +10,7 @@ class SubscriptionView(ReadOnlyModelViewSet):
     # queryset = Subscription.objects.all().prefetch_related('client').prefetch_related('client__user')
     queryset = Subscription.objects.all().prefetch_related(
         'plan',  # для оптимизации вложенного серализатора(проблема n+1)
+        'service',
         Prefetch('client', queryset=Client.objects.all().select_related('user').only('company_name', 'user__email')))
     serializer_class = SubscriptionSerializer
 
